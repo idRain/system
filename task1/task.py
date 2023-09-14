e7fb230db10879dfce68b0dbfb96ed2403f350dc
idRain <73492054+idRain@users.noreply.github.com>
2023-09-14 17:09:59 +0400

    update task.py

--- task1/task.py
+++ task1/task.py
@@ -3,10 +3,11 @@ import csv
 
 def main():
   file = sys.argv[1]
-  row = sys.argv[2]
-  col = sys.argv[3]
+  row = int(sys.argv[2])
+  col = int(sys.argv[3])
 
   with open(file) as f:
     data = list(csv.reader(f, delimiter=","))
     print(data[row][col])
-      
+
+main()
